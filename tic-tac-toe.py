e558dcc7ce7c859c67ee86d19910bf17f7060083
jaewon <cjw1090@github.com>
2017-11-24 22:40:42 -0800

    completion

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -10,20 +10,48 @@ def checked(i) :
       button["bg"] = "yellow"
 
       if player == "X" :
+            check(i)
             player = "O"
             button["bg"] = "yellow"
       else :
+            check(i)
             player = "X"
             button["bg"] = "lightgreen"
+
+
 
 window = Tk()
 player = "X"
 list= []
 
+
 for i in range(9) :
       b = Button(window, text="     ", command=lambda k=i: checked(k))
       b.grid(row=i//3, column=i%3)
       list.append(b)
+
+def check(i) :
+      r = i//3
+      c = i%3
+
+      if list[r*3]["text"] == list[r*3+1]["text"] == list[r*3+2]["text"] :
+            win()
+      elif list[c]["text"] == list[c+3]["text"] == list[c+6]["text"] :
+            win()
+      elif list[0]["text"] == list[4]["text"] == list[8]["text"] != "     " or list[2]["text"] == list[4]["text"] == list[6]["text"] != "     " :
+            win()
+
+def win() :
+      if player=="X":
+            msg = Message(window, text="X Win")
+            msg.grid(row=3, column=0)
+
+            
+      else :
+            msg = Message(window, text="O Win")
+            msg.grid(row=3, column=0)
+
+            
 
 window.mainloop()
 
